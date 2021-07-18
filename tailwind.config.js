68ace44430836d4eebb90fe60bca07b5acce2bf2
Luca Martinelli <martinelliluca98@gmail.com>
2021-07-18 17:46:56 +0200

    Initial release

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  /*purge: {
+    enabled: true,
+    content: ["./*.html"],
+  },*/
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        black: "#333333",
+        lightblack: "#787878",
+        background: "#fdf6f0",
+        link: "#be5fe8",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans"],
+        serif: ["Otomanopee One", "sans-serif"],
+        mono: ["Cascadia Code", "monospace", "ui-monospace"],
+      },
+      screens: {
+        '3xl': '1720px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
