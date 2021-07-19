aa34dd694e8ed330da421e41632a8951010d7724
Luca Martinelli <martinelliluca98@gmail.com>
2021-07-19 18:09:32 +0200

    Menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-$(".menu-toggle").click(function(e) {
-    e.preventDefault();
+$(".menu-toggle").click(function (e) {
+  e.preventDefault();
 
-    $($(this).data("ref")).toggleClass("menu-opened");
-    $(this).toggleClass("menu-opened");
-    $("body").toggleClass("overflow-hidden");
+  $("#" + $(this).data("ref")).toggleClass("menu-opened");
+  $(this).toggleClass("menu-opened");
+  $("body").toggleClass("overflow-hidden");
+});
+
+$("#nav").click(function () {
+  $(".menu-toggle[data-ref='" + $(this).attr("id") + "']").removeClass("menu-opened");
+  $(this).removeClass("menu-opened");
+  $("body").removeClass("overflow-hidden");
 });
