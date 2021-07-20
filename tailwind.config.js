200caa3ecd50ff964b4ca957cfe2bccf8402181d
Luca Martinelli <martinelliluca98@gmail.com>
2021-07-20 22:16:01 +0200

    FIxed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ["./*.html", "./*.js"],
+    content: ["./*.html", "./js/*.js", "./js/**/*.js"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
