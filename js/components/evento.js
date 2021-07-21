d884e63360c52d382c99e1e5e872a7199a0b97d0
Luca Martinelli <martinelliluca98@gmail.com>
2021-07-21 14:05:19 +0200

    Fixed

--- js/components/evento.js
+++ js/components/evento.js
@@ -1,22 +1,27 @@
 function createEventCard(evento) {
-    const eventLocation = evento.location;
-    const eventLocationUrl = "https://www.google.com/maps/search/" + encodeURIComponent(evento.location);
-    const startEventDate = evento.start.dateTime;
-    const endEventDate = evento.end.dateTime;
-    const eventTitle = evento.summary;
-    const eventDescription = evento.description;
-    const eventUrl = evento.htmlLink;
-    const eventoImg = ""; // evento.imgUrl;
+  const eventLocation = evento.location;
+  const eventLocationUrl = "https://www.google.com/maps/search/" + encodeURIComponent(evento.location);
+  const startEventDate = evento.start.dateTime;
+  const endEventDate = evento.end.dateTime;
+  const eventTitle = evento.summary;
+  const eventDescription = evento.description;
+  const eventUrl = evento.htmlLink;
+  const eventoImg = evento.imgUrl;
 
-    return `<div class="card event">
-                <div class="card-image">
-                    <img src="${eventoImg}" alt="${eventTitle}" />
-                </div>
+  const imgElement = eventoImg != null ? `<div class="card-image"><img src="${eventoImg}" alt="${eventTitle}" /></div>` : "";
+  const descriptionElement = eventDescription != undefined && eventDescription != null ? `<p>${eventDescription}</p>` : "";
+  const locationElement =
+    eventLocation != undefined && eventLocation != null
+      ? `<span class="info-element"><i class="feather icon-map-pin location" aria-hidden="true"></i><a href="${eventLocationUrl}" target="_blank" rel="noopener noreferrer">${eventLocation}</a></span>`
+      : "";
+
+  return `<div class="card event">
+                ${imgElement}
                 <div class="card-content">
                     <h2>${eventTitle}</h2>
-                    <p>${eventDescription}</p>
+                    ${descriptionElement}
                     <div class="list-information">
-                        <span class="info-element"><i class="feather icon-map-pin location" aria-hidden="true"></i><a href="${eventLocationUrl}">${eventLocation}</a></span>
+                        ${locationElement}
                         <span class="info-element"><i class="feather icon-clock time" aria-hidden="true"></i>${startEventDate}-${endEventDate}</span>
                     </div>
                     <a href="${eventUrl}" role="button" class="button m-0">Più informazioni</a>
