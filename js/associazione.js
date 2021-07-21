e0063b89e00e5be43b54d48783416de8dc86d21e
Luca Martinelli <martinelliluca98@gmail.com>
2021-07-21 17:15:24 +0200

    Fixed bug

--- js/associazione.js
+++ js/associazione.js
@@ -1,38 +1,39 @@
 var allTags;
 var contactTypes;
 
-var assID = getTokens("/associazioni/")[0];
-
 function listAssociazione(assID) {
-    const spinner = spinnerElement.clone();
-    $("#associazione").empty().append(spinner);
+  const spinner = spinnerElement.clone();
+  $("#associazione").empty().append(spinner);
 
-    $.ajax({
-      url: baseSito + "associazioni.json",
-      type: "GET",
-      complete: function () {
-        $("#associazione .spinner").remove();
-      },
-      success: function (data) {
-        allTags = data.tags;
-        contactTypes = data.contactTypes;
+  $.ajax({
+    url: baseSito + "associazioni.json",
+    type: "GET",
+    complete: function () {
+      $("#associazione .spinner").remove();
+    },
+    success: function (data) {
+      allTags = data.tags;
+      contactTypes = data.contactTypes;
 
-        if (data.associazioni.hasOwnProperty(assID)) {
-          const associazione = data.associazioni[assID];
+      if (data.associazioni.hasOwnProperty(assID)) {
+        const associazione = data.associazioni[assID];
 
-          const assElement = createAssociazioneFullCard(associazione);
-          $("#associazione").empty().append("<h1>Scheda associazione</h1>").append(assElement);
+        const assElement = createAssociazioneFullCard(associazione);
+        $("#associazione").empty().append("<h1>Scheda associazione</h1>").append(assElement);
 
-          if (associazione.contributi != null && associazione.contributi.length > 0) {
-            $("#contributi").empty().append("<h2 class='mt-16 mb-10'>Contributi da P.A.</h2><div class='events-container'></div>");
+        if (associazione.contributi != null && associazione.contributi.length > 0) {
+          $("#contributi").empty().append("<h2 class='mt-16 mb-10'>Contributi da P.A.</h2><div class='events-container'></div>");
 
-            associazione.contributi.forEach((contributo) => {
-              const contributoElement = createContributoCard(contributo);
+          associazione.contributi.forEach((contributo) => {
+            const contributoElement = createContributoCard(contributo);
 
-              $("#contributi .events-container").append(contributoElement);
-            });
-          }
+            $("#contributi .events-container").append(contributoElement);
+          });
         }
-      },
-    });
+      }
+    },
+  });
 }
+
+var assID = getTokens("/associazioni/")[0];
+listAssociazione(assID);
